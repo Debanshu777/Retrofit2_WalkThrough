1864496b81951e408417a598b93c18da7fc1bfc3
Debanshu Datta <debanshudatta123@gmail.com>
2020-05-19 03:08:52 +0530

    Post Request and Form Urlencoded

--- app/src/main/java/com/example/retrofit2_walkthrough/MainActivity.java
+++ app/src/main/java/com/example/retrofit2_walkthrough/MainActivity.java
@@ -31,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
         jsonPlaceHolderApi= retrofit.create(JsonPlaceHolderApi.class);
         //getPosts();
-        getComments();
+        //getComments();
+        createPosts();
     }
     private void getPosts(){
 
@@ -112,5 +113,44 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    private void createPosts(){
+        //for first case normal post
+       // Post post=new Post(23,"New Title","New Text");
 
+
+        //in case of @FromUrlEncoded
+        //Call<Post> call=jsonPlaceHolderApi.createPost(23,"New Title","New Text");
+
+        //in case of@FromUrlEncoded array
+        Map<String,String> fields=new HashMap<>();
+        fields.put("userId","25");
+        fields.put("title","New Title");
+        Call<Post> call=jsonPlaceHolderApi.createPost(fields);
+
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                if(!response.isSuccessful()){
+                    textView.setText("Code: "+response.code());
+                    return;
+                }
+                Post postResponse =response.body();
+                String content="";
+                content+="Code: "+ response.code()+"\n";
+                content+="ID: "+ postResponse.getId()+"\n";
+                content+="User ID: " +postResponse.getUserId()+"\n";
+                content+="Title: "+postResponse.getTitle()+"\n";
+                content+="Text: "+postResponse.getText()+"\n\n";
+
+                textView.append(content);
+
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+                textView.setText(t.getMessage());
+            }
+        });
+
+    }
 }
--- app/src/main/java/com/example/retrofit2_walkthrough/Post.java
+++ app/src/main/java/com/example/retrofit2_walkthrough/Post.java
@@ -4,14 +4,18 @@ import com.google.gson.annotations.SerializedName;
 //This is the java object that we want to create from the after GSON converter
 
 public class Post {
-    //these are all the keys that we ant to extract from the JSON object
     private  int userId;
-    private  int id;
+    private  Integer id;
     private String title;
-    //this annotation helps to identify which key to refer if our variable name is different
     @SerializedName("body")
     private String text;
-    //generated getter methods for the variable
+
+    public Post(int userId, String title, String text) {
+        this.userId = userId;
+        this.title = title;
+        this.text = text;
+    }
+
     public int getUserId() {
         return userId;
     }
